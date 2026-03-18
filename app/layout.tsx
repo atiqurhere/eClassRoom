4b955fcf6eb75673dfb6d32766a2b6a98fd04f62
atiqurhere <atiqur.dev404@gmail.com>
2026-03-18 02:11:32 +0000

    fix: suppress body hydration warning from browser extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="antialiased">
+      <body className="antialiased" suppressHydrationWarning>
         {children}
         <Toaster position="top-right" richColors />
         <script
